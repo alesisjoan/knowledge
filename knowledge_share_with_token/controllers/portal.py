f87f21a06819549b57bb62449bceb5d13ea6a69f
alesisjoan <alesis4ta@gmail.com>
2024-06-17 15:57:30 -0300

    [REF] knowledge_share_with_token: refactor code, added access_token to session data

--- knowledge_share_with_token/controllers/portal.py
+++ knowledge_share_with_token/controllers/portal.py
@@ -1,11 +1,16 @@
-import werkzeug
-
 from odoo import http
 from odoo.http import request
 from odoo.addons.knowledge.controllers.main import KnowledgeController
 
 
 class CustomKnowledgeWebsiteController(KnowledgeController):
+
+    def _get_article_with_token(self, article_id, access_token):
+        article = request.env['knowledge.article'].sudo().search([('id', '=', article_id)])
+        if article and access_token and article.share_with_token:
+            article = article._get_articles_and_check_access(access_token)
+            return article
+        return False
 
     @http.route('/knowledge/article/<int:article_id>/<string:access_token>', type='http', auth='public', website=True)
     def redirect_to_article_with_token(self, **kwargs):
@@ -14,15 +19,11 @@ class CustomKnowledgeWebsiteController(KnowledgeController):
         if 'access_token' in kwargs and 'article_id' in kwargs:
             article_id = kwargs['article_id']
             access_token = kwargs['access_token']
-            article = request.env['knowledge.article'].sudo().search([('id', '=', article_id)])
-            if article and access_token and article.share_with_token:
-                available_documents = article._get_documents_and_check_access(access_token)
-                if available_documents is False:
-                    return request.not_found()
-                if request.env.user.has_group('base.group_user'):
-                    if not article:
-                        return werkzeug.exceptions.Forbidden()
-                    return self._redirect_to_backend_view(article)
-                return self._redirect_to_portal_view(article)
-            return werkzeug.exceptions.Forbidden()
+            article = self._get_article_with_token(article_id, access_token)
+            request.session.data = {'knowledge_access_token': access_token}
+            if not article:
+                return request.not_found()
+            if request.env.user.has_group('base.group_user'):
+                return self._redirect_to_backend_view(article)
+            return self._redirect_to_portal_view(article)
 
