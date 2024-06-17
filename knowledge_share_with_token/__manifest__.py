046707c55523cfb32749d7c2694da964e62eda0b
alesisjoan <alesis4ta@gmail.com>
2024-06-17 15:56:50 -0300

    [FIX] knowledge_share_with_token: fix tree view of article shared with token

--- knowledge_share_with_token/__manifest__.py
+++ knowledge_share_with_token/__manifest__.py
@@ -8,10 +8,10 @@
     "version": "16.0.1.0.0",
     "license": "LGPL-3",
     "depends": [
-        "knowledge",
         "website_knowledge"
     ],
     "data": [
-      "views/knowledge_article_views.xml",
+        "views/knowledge_article_views.xml",
+        "views/knowledge_templates_common.xml",
     ],
 }
